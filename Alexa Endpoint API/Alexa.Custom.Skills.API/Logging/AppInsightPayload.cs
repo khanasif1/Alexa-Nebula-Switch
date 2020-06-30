4f3e57db4a64b18f5fb0718e8c0ce0669aaf0c05
Asif Khan <askha@microsoft.com>
2020-06-30 17:19:37 +1000

    Alexa API backend using Azure Functions

---
+++ Alexa Endpoint API/Alexa.Custom.Skills.API/Logging/AppInsightPayload.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Alexa.Custom.Skills.API.Logging
+{
+    public class AppInsightPayload
+    {
+        public string _operation { get; set; }
+        public AppInsightLanguage _type { get; set; }
+        public string _correlationId { get; set; }
+        public string _payload { get; set; }
+        public Exception _ex { get; set; }
+    }
+}
