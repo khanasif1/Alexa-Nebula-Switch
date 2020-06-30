75aff07cbefbc0c81f0770f5dea5015f8fe5e955
Asif Khan <askha@microsoft.com>
2020-06-30 22:13:44 +1000

    moving API Function to root folder

---
+++ Alexa.Custom.Skills.API/Logging/AppInsightHelper.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.ApplicationInsights;
+using Microsoft.ApplicationInsights.DataContracts;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+
+namespace Alexa.Custom.Skills.API.Logging
+{
+    public sealed class AppInsightHelper
+    {
+        private static AppInsightHelper _instance;
+        private AppInsightHelper() { }
+
+        public static AppInsightHelper Instance
+        {
+            get
+            {
+                if (_instance == null)
+                {
+                    _instance = new AppInsightHelper();
+                }
+                return _instance;
+            }
+        }
+        private static TelemetryClient telemetryClient;
+        public void AppInsightInit(AppInsightPayload _payload)
+        {
+
+            IServiceCollection services = new ServiceCollection();
+
+            services.AddApplicationInsightsTelemetryWorkerService("{Application Insight Key}");
+
+            IServiceProvider serviceProvider = services.BuildServiceProvider();            
+
+            telemetryClient = serviceProvider.GetRequiredService<TelemetryClient>();
+
+            using (telemetryClient.StartOperation<RequestTelemetry>(_payload._operation))
+            {               
+                
+                if (_payload._type.CompareTo(AppInsightLanguage.AppInsightEvent) == 0)
+                {
+                    EventTelemetry _evt = new EventTelemetry();                    
+                    _evt.Context.Operation.Id = _payload._correlationId;
+                    _evt.Name = _payload._payload;    
+                    telemetryClient.TrackEvent(_payload._payload);
+                }                    
+                if (_payload._type.CompareTo(AppInsightLanguage.AppInsightTrace) == 0)
+                {
+                    TraceTelemetry _trct = new TraceTelemetry();
+                    _trct.Message = _payload._payload;
+                    _trct.Context.Operation.Id = _payload._correlationId;
+
+                    telemetryClient.TrackTrace(_trct);
+                }                    
+                if (_payload._type.CompareTo(AppInsightLanguage.AppInsightException) == 0)
+                {
+                    ExceptionTelemetry _et = new ExceptionTelemetry();
+                    _et.Exception = _payload._ex;
+                    _et.Context.Operation.Id = _payload._correlationId;                    
+                    telemetryClient.TrackException(_et);
+                }
+                    
+            }
+            telemetryClient.Flush();
+
+        }
+    }
+    public enum AppInsightLanguage
+    {
+        AppInsightEvent,
+        AppInsightTrace,
+        AppInsightException
+    }
+}
